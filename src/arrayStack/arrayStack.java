696e34ca67c1d5e7640e0bdf5291ae50620b397c
5018b <5018b@DESKTOP-LABV991>
2018-07-09 19:48:41 +0900

    배열 스택 구현

---
+++ src/arrayStack/arrayStack.java
@@ -0,0 +1,67 @@
+package arrayStack;
+
+
+public class arrayStack {
+   
+   public class Node {
+      int data = 0;
+   }
+   
+   public class ArrayStack {
+      int capacity; //스택 용량
+      int top; //노드 배열 최상위 인덱스
+      Node Nodes[]; //노드 배열
+   }
+   
+   /**
+    * 스택 생성 
+    **/
+   public ArrayStack createStact(ArrayStack stack, int capacity) {
+      stack = new ArrayStack(); //스택 메모리 생성
+      stack.Nodes = new Node[capacity]; //capacity만큼의 노드 메모리 설정
+      stack.capacity = capacity; //입력된 capacity로 스택 용량 설정
+      stack.top = 0; //top 초기화
+      
+      return stack;
+   }
+   
+   /**
+    * 스택 데이터 저장
+    **/
+   public void Push(ArrayStack stack, int data) {
+      stack.Nodes[stack.top] = new Node();
+      stack.Nodes[stack.top++].data = data;
+   }
+   
+   /**
+     * 스택 data 반환 
+    **/
+   public int Pop(ArrayStack stack) {
+      return stack.Nodes[--stack.top].data;
+   }
+   
+   /**
+     * 스택 메모리 갯수 반환 
+    **/
+   public int getSize(ArrayStack stack) {
+      return stack.top;
+   }
+   
+   /**
+     * 스택 비어져있는지 여부 반환 
+    **/
+   public boolean isEmpty(ArrayStack stack) {
+      return stack.top == 0;
+   }
+   
+   /**
+     * 스택 상태 출력 
+    **/
+   public void printStackStatus(ArrayStack stack) {
+      System.out.println("print stack status...\n");
+      System.out.println("capacity : " + stack.capacity);
+      System.out.println("top : " + stack.top);
+      System.out.println("stack size : " + getSize(stack));
+      System.out.println("stack is empty? : " + isEmpty(stack));
+   }
+}
---
+++ src/arrayStack/test_arrayStack.java
@@ -0,0 +1,41 @@
+package arrayStack;
+
+import arrayStack.arrayStack.ArrayStack;
+
+public class test_arrayStack {
+   public static void main(String args[]) {
+      arrayStack as = new arrayStack();
+      ArrayStack stack = null;
+      
+      /*스택 생성*/
+      System.out.println("create stack..\n");
+      stack = as.createStact(stack, 10); //10개의 데이터를 담을 스택 생성
+      
+      /*데이터 삽입*/
+      System.out.println("push data...\n");
+      for(int i=0; i<5; i++) {
+         as.Push(stack, i); //스택에 i값을 차례대로 삽입
+      }
+      
+      /*현재 스택 상태 출력*/
+      as.printStackStatus(stack);
+      
+      /*데이터 반환*/
+      System.out.println("pop data....\n");
+      
+      for(int i=0; i<10; i++) {
+         if(as.isEmpty(stack)) {
+            System.out.println(i + " stack is empty...\n");
+            break;
+         }
+         
+         System.out.println(i + " pop : " + as.Pop(stack));
+      }
+      
+      /*현재 스택 상태 출력*/
+      as.printStackStatus(stack);
+      
+      
+   }
+
+}
